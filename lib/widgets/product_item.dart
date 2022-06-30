1ddbf8c248f236ce426e608f615c187bad86b5bd
Andrew Thow <thow1976@hotmail.co.uk>
2022-06-30 15:00:27 +0100

    7_191_Setting_Up_Named_Screen_Routes

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -2,38 +2,48 @@ import 'dart:ffi';
 
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
+import '../screens/product_details_screen.dart';
 import 'package:flutter/material.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
-  final String imageURL;
+  final String imageUrl;
 
-  ProductItem(this.id, this.title, this.imageURL);
+  ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: Image.network(
-          imageURL,
-          fit: BoxFit.cover,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: id,
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
             icon: Icon(Icons.favorite),
-            onPressed: () {},
             color: Theme.of(context).accentColor,
+            onPressed: () {},
           ),
           title: Text(
             title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
+            icon: Icon(
+              Icons.shopping_cart,
+            ),
             onPressed: () {},
             color: Theme.of(context).accentColor,
           ),
