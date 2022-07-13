e1addd033921ec00b036457bb709f8290cc0b8e0
Andrew Thow <thow1976@hotmail.co.uk>
2022-07-13 11:38:46 +0100

    25_211_Adding_Orders_Screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,10 +1,8 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/cart.dart';
-import 'package:shop_app/widgets/cart_item.dart';
+
+import '../providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 import '../providers/orders.dart';
 
 class CartScreen extends StatelessWidget {
@@ -17,58 +15,60 @@ class CartScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Your Cart'),
       ),
-      body: Column(children: <Widget>[
-        Card(
-          margin: EdgeInsets.all(15),
-          child: Padding(
-            padding: EdgeInsets.all(8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  'Total',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Spacer(),
-                Chip(
-                  label: Text(
-                    '\$${cart.totalAmount}',
-                    style: TextStyle(
-                      color:
-                          Theme.of(context).primaryTextTheme.headline6!.color,
-                    ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Total',
+                    style: TextStyle(fontSize: 20),
                   ),
-                  backgroundColor: Theme.of(context).primaryColor,
-                ),
-                FlatButton(
-                  child: Text('ORDER NOW'),
-                  onPressed: () {
-                    Provider.of<Orders>(context, listen: false)
-                        .addOrder(cart.items.values.toList(), cart.totalAmount);
-
-                    cart.clear();
-                  },
-                  textColor: Theme.of(context).primaryColor,
-                )
-              ],
+                  Spacer(),
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount}',
+                      style: TextStyle(
+                        color:
+                            Theme.of(context).primaryTextTheme.headline6!.color,
+                      ),
+                    ),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  FlatButton(
+                    child: Text('ORDER NOW'),
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                        cart.items.values.toList(),
+                        cart.totalAmount,
+                      );
+                      cart.clear();
+                    },
+                    textColor: Theme.of(context).primaryColor,
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: cart.items.length,
-            itemBuilder: (ctx, i) => CartItemList(
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (ctx, i) => CartItemList(
                 cart.items.values.toList()[i].id,
                 cart.items.keys.toList()[i],
                 cart.items.values.toList()[i].price,
                 cart.items.values.toList()[i].quantity,
-                cart.items.values.toList()[i].title),
-          ),
-        )
-      ]),
+                cart.items.values.toList()[i].title,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
