a743b442062d36fd006342582d006b50c89c6e32
Andrew Thow <thow1976@hotmail.co.uk>
2022-07-21 14:48:42 +0100

    1_232_Saving_New_Products

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import '../providers/product.dart';
 import '../providers/products_providers.dart';
-import 'package:provider/provider.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/edit_product_screen.dart';
+import '../providers/products_providers.dart';
 
 class UserProductItem extends StatelessWidget {
   final String title;
